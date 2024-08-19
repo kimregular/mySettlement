c756a72e45556382c11c73b81fd6d7fa0b69f602
kimregular <regular.jk.kim@gmail.com>
2024-08-19 14:32:56 +0900

    remove : unnecessary codes

--- src/main/java/com/mysettlement/member/controller/MemberController.java
+++ src/main/java/com/mysettlement/member/controller/MemberController.java
@@ -1,9 +1,7 @@
 package com.mysettlement.member.controller;
 
 import com.mysettlement.member.entity.Member;
-import com.mysettlement.member.entity.MemberRole;
 import com.mysettlement.member.repository.MemberRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,13 +22,5 @@ public class MemberController {
     @GetMapping("/api/member")
     public List<Member> getAllMembers() {
         return memberRepository.findAll();
-    }
-
-    @PostConstruct
-    public void init() {
-        for (int i = 0; i < 10; i++) {
-            memberRepository.save(Member.of(("member" + i), ("member" + i + "@test.test"), MemberRole.DEFAULT));
-        }
-        memberRepository.save(Member.of("방망이깎는노인", "regular.jk.kim@gmail.com", MemberRole.DEFAULT));
     }
 }

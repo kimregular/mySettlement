d0b830e852cdd609d13fe09ed4395412887278ef
kimregular <regular.jk.kim@gmail.com>
2024-08-22 16:49:14 +0900

    유저 데이터 추가 쿼리

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO USERS
+    (name, email, password, user_role)
+VALUES ('kim', 'regular@test.com', '1234', 'UPLOADER'),
+       ('lee', 'lee@test.com', '1234', 'UPLOADER'),
+       ('park', 'park@test.com', '1234', 'DEFAULT'),
+       ('choi', 'choi@test.com', '1234', 'GUEST'),
+       ('jung', 'jung@test.com', '1234', 'UPLOADER'),
+       ('yoon', 'yoon@test.com', '1234', 'GUEST'),
+       ('han', 'han@test.com', '1234', 'DEFAULT'),
+       ('kang', 'kang@test.com', '1234', 'DEFAULT'),
+       ('seo', 'seo@test.com', '1234', 'GUEST'),
+       ('jang', 'jang@test.com', '1234', 'UPLOADER');

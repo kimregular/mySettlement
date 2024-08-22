3d5ac2606e62faada92100a6a125ef8497a23683
kimregular <regular.jk.kim@gmail.com>
2024-08-22 16:47:48 +0900

    유저 서비스 추가

---
+++ src/main/java/com/mysettlement/user/service/UserService.java
@@ -0,0 +1,46 @@
+package com.mysettlement.user.service;
+
+import com.mysettlement.user.entity.User;
+import com.mysettlement.user.entity.UserRole;
+import com.mysettlement.user.exception.DuplicateUserException;
+import com.mysettlement.user.exception.NoUserFoundException;
+import com.mysettlement.user.repository.UserRepository;
+import com.mysettlement.user.request.UserSigninRequestDto;
+import com.mysettlement.user.response.UserResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+    public UserResponseDto findByUserName(String name) {
+        User user = userRepository.findByName(name).orElseThrow(NoUserFoundException::new);
+        return UserResponseDto.of(user);
+    }
+
+    public UserResponseDto signinUser(UserSigninRequestDto userSigninRequestDto) {
+        if (isExistUser(userSigninRequestDto)) {
+            throw new DuplicateUserException();
+        }
+
+        User newUser = User.builder()
+                .name(userSigninRequestDto.getName())
+                .email(userSigninRequestDto.getEmail())
+                .password(userSigninRequestDto.getPassword())
+                .userRole(UserRole.GUEST)
+                .build();
+        userRepository.save(newUser);
+        return UserResponseDto.of(newUser);
+    }
+
+    private boolean isExistUser(UserSigninRequestDto userSigninRequestDto) {
+        return userRepository.findByEmail(userSigninRequestDto.getEmail()).isPresent();
+    }
+
+}

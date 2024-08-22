8af495f9272e5113277515ae5c0a841ec5d2535a
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-08-22 22:59:12 +0900

    비디오 업로드 dto 제작

---
+++ src/main/java/com/mysettlement/video/response/VideoResponseDto.java
@@ -0,0 +1,13 @@
+package com.mysettlement.video.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class VideoResponseDto {
+
+    private final String title;
+    private final String desc;
+
+}

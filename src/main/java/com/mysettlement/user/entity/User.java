20c6891e2ff9fbf3f441f1f2e2d24296d2208b32
kimregular <regular.jk.kim@gmail.com>
2024-08-22 16:43:24 +0900

    유저 엔티티 추가

---
+++ src/main/java/com/mysettlement/user/entity/User.java
@@ -0,0 +1,38 @@
+package com.mysettlement.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.EnumType.*;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@Table(name = "USERS")
+@NoArgsConstructor(access = PROTECTED)
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @Column(name = "user_role")
+    @Enumerated(STRING)
+    private UserRole userRole;
+
+
+    @Builder
+    private User(String name, String email, String password, UserRole userRole) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.userRole = userRole;
+    }
+}

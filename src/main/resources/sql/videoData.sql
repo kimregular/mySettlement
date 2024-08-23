496364629580c908e914b540c5d3762821b2330f
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-08-23 14:10:33 +0900

    비디오 초기값 삽입 sql

---
+++ src/main/resources/sql/videoData.sql
@@ -0,0 +1,4 @@
+INSERT INTO VIDEO
+    (user_id, video_title, video_view, video_desc, video_status, video_price_per_view)
+VALUES
+    (1, 'testVideo1', 0, 'test video for test 1', 'AVAILABLE', 1.5);

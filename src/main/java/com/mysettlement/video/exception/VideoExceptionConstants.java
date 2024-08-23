5a3cdac9a818c176f39c6230d5f93c55ff3386d7
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-08-23 11:23:08 +0900

    비디오 예외처리 핸들러 추가

--- src/main/java/com/mysettlement/video/exception/VideoExceptionConstants.java
+++ src/main/java/com/mysettlement/video/exception/VideoExceptionConstants.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum VideoExceptionConstants {
 
-    INVALID_VIDEO_UPLOAD_EXCEPTION("유효하지 않은 업로드 요청입니다.", HttpStatus.BAD_REQUEST);
+    INVALID_VIDEO_UPLOAD_EXCEPTION("유효하지 않은 업로드 요청입니다.", HttpStatus.BAD_REQUEST),
+    UPLOADER_ROLE_REQUIRED_EXCEPTION("업로더만 비디오 업로드가 가능합니다.", HttpStatus.UNAUTHORIZED)
+    ;
 
     private final String MESSAGE;
     private final HttpStatus status;

2464932c6ecab23b093df0636619d3800b17d40a
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-08-24 11:41:55 +0900

    광고 상태 변경 api #7
    광고 상태 변경 api 기능 구현 완료

--- src/main/java/com/mysettlement/ad/controller/AdController.java
+++ src/main/java/com/mysettlement/ad/controller/AdController.java
@@ -1,6 +1,8 @@
 package com.mysettlement.ad.controller;
 
+import com.mysettlement.ad.exception.InvalidAdUpdateRequestException;
 import com.mysettlement.ad.exception.InvalidAdUploadRequestException;
+import com.mysettlement.ad.request.AdStatusUpdateReqeustDto;
 import com.mysettlement.ad.request.AdUploadRequestDto;
 import com.mysettlement.ad.response.AdResponseDto;
 import com.mysettlement.ad.service.AdService;
@@ -27,5 +29,13 @@ public class AdController {
     public MySettlementGlobalResponse<AdResponseDto> uploadAd(@Valid @RequestBody AdUploadRequestDto adUploadRequestDto, BindingResult errors) {
         if(errors.hasErrors()) throw new InvalidAdUploadRequestException(errors);
         return MySettlementGlobalResponse.of(HttpStatus.OK, adService.uploadAd(adUploadRequestDto));
+    }
+
+    @PatchMapping("/{adId}")
+    public MySettlementGlobalResponse<AdResponseDto> updateAd(@PathVariable Long adId,
+                                                              @Valid @RequestBody AdStatusUpdateReqeustDto adStatusUpdateReqeustDto,
+                                                              BindingResult errors) {
+        if(errors.hasErrors()) throw new InvalidAdUpdateRequestException(errors);
+        return MySettlementGlobalResponse.success(adService.changeStatus(adId, adStatusUpdateReqeustDto));
     }
 }
---
+++ src/main/java/com/mysettlement/ad/exception/InvalidAdUpdateRequestException.java
@@ -0,0 +1,29 @@
+package com.mysettlement.ad.exception;
+
+import com.mysettlement.globalException.MySettlementException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import static com.mysettlement.ad.exception.AdExceptionConstants.*;
+
+public class InvalidAdUpdateRequestException extends MySettlementException {
+
+    private static final String MESSAGE = INVALID_AD_UPDATE_REQUEST_EXCEPTION.getMessage();
+
+    public InvalidAdUpdateRequestException() {
+        super(MESSAGE);
+    }
+
+    public InvalidAdUpdateRequestException(BindingResult errors) {
+        super(MESSAGE);
+        for (FieldError fieldError : errors.getFieldErrors()) {
+            addValidation(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+
+    @Override
+    public HttpStatus getStatusCode() {
+        return INVALID_AD_UPDATE_REQUEST_EXCEPTION.getStatus();
+    }
+}

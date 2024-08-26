c7cc7be9452fe54a65e20ea63cf31d491c2d3409
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-08-26 18:45:27 +0900

    시청 기록 엔티티 제작 #22
    테스트용 데이터 정리

--- src/test/resources/sql/userData.sql
+++ src/test/resources/sql/userData.sql
@@ -1,12 +1,4 @@
 INSERT INTO USERS
     (name, email, password, user_role)
 VALUES ('kim', 'regular@test.com', '1234', 'DEFAULT'),
-       ('lee', 'lee@test.com', '1234', 'GUEST'),
-       ('park', 'park@test.com', '1234', 'DEFAULT'),
-       ('choi', 'choi@test.com', '1234', 'GUEST'),
-       ('jung', 'jung@test.com', '1234', 'DEFAULT'),
-       ('yoon', 'yoon@test.com', '1234', 'GUEST'),
-       ('han', 'han@test.com', '1234', 'DEFAULT'),
-       ('kang', 'kang@test.com', '1234', 'DEFAULT'),
-       ('seo', 'seo@test.com', '1234', 'GUEST'),
-       ('jang', 'jang@test.com', '1234', 'DEFAULT');
+       ('lee', 'lee@test.com', '1234', 'GUEST');

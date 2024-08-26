94612e3b7cf31c6588775011da71ccdb2a4cb82e
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-08-26 13:47:41 +0900

    컨트롤러 BindingResult 제거하기 #29
    예외 처리 핸들러 수정

--- src/main/java/com/mysettlement/globalException/MySettlementGlobalExceptionHandler.java
+++ src/main/java/com/mysettlement/globalException/MySettlementGlobalExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.mysettlement.globalException;
 
 import com.mysettlement.globalResponse.MySettlementGlobalErrorResponse;
-import com.mysettlement.user.exception.InvalidSigninRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,12 +17,6 @@ public class MySettlementGlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<MySettlementGlobalErrorResponse> handleJsonMappingException(MethodArgumentNotValidException e) {
-        InvalidSigninRequestException invalidSigninRequestException = new InvalidSigninRequestException();
-        for (ObjectError allError : e.getAllErrors()) {
-            String fieldName = ((FieldError) allError).getField();
-            String errorMessage = allError.getDefaultMessage();
-            invalidSigninRequestException.addValidation(fieldName, errorMessage);
-        }
-        return new ResponseEntity<>(MySettlementGlobalErrorResponse.of(invalidSigninRequestException), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(MySettlementGlobalErrorResponse.of(e), HttpStatus.BAD_REQUEST);
     }
 }
